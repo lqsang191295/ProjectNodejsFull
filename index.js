4c030d23922d3f6af3bc459cafc8e862639efad6
Le Quang Sang <lqsang1995@gmail.com>
2019-08-09 17:52:49 +0700

    2

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const appConfig = require('./config') || {};
 const PORT = appConfig.PORT || 2345;
 const routers = require('./routers');
 const logger = require('./utils/logger');
-console.log('routers', routers)
+
 app.use(routers)
 
 app.listen(PORT, () => {
+    console.log(PORT)
     logger.info('Listen port %s', PORT)
 })
