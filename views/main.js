37ecc58ac9dd51ee320c7464c3babda4d3741a2c
Le Quang Sang <lqsang1995@gmail.com>
2019-08-14 10:17:09 +0700

    update

---
+++ views/main.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    },
+    mounted () {
+        // axios
+        //     .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+        //     .then(response => (this.info = response))
+    }
+})
